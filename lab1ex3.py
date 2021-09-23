32f2740818e120d6cbaca7cf11748b49f7d5d827
NikolaySieviertsev <90578332+NikolaySieviertsev@users.noreply.github.com>
2021-09-24 00:30:35 +0300

    Ex3 changed

--- lab1ex3.py
+++ lab1ex3.py
@@ -17,114 +17,26 @@ user_input = '' result = (False, None)
 """
 
 import sys
+from ultrasplit import split_by_symbol
 
 
-def is_valid(expr: dict):
-    return expr["valid"]
+def check_result(term, index=0):
+    math_sign = "+-*/"
+    if term[0] not in math_sign:
+        term = "+" + term
 
-
-def literal(index: list):
-    if index[0] == len(sys.argv):
-        return {"valid": False, "result": None}
-
-    argument = sys.argv[index[0]]
-
-    index[0] += 1
-
-    if argument.isdigit():
-        number = {"valid": True, "result": int(argument)}
-
-        return number
-    else:
-        return {"valid": False, "result": None}
-
-
-def multiplicative(index: list):
-    num_1 = literal(index)
-
-    if not is_valid(num_1):
-        return {"valid": False, "result": None}
-
-    while True:
-        if index[0] == len(sys.argv):
-            break
-
-        argument = sys.argv[index[0]]
-
-        if argument == "*":
-            index[0] += 1
-
-            num_2 = literal(index)
-
-            if not is_valid(num_2):
-                return {"valid": False, "result": None}
-
-            num_1 = {"valid": True, "result": num_1["result"] * num_2["result"]}
-
-            continue
-        elif argument == "/":
-            index[0] += 1
-
-            num_2 = literal(index)
-
-            if not is_valid(num_2):
-                return {"valid": False, "result": None}
-
-            num_1 = {"valid": True, "result": num_1["result"] / num_2["result"]}
-
-            continue
+    if not isinstance(term, list):
+        term = split_by_symbol(term)
+    try:
+        if term[index] in math_sign:
+            if (term[index + 1]).isdigit():
+                return check_result(term, index + 2)
+            return False, None
         else:
-            break
-
-    return num_1
-
-
-def additive(index: list):
-    num_1 = multiplicative(index)
-
-    if not is_valid(num_1):
-        return {"valid": False, "result": None}
-
-    while True:
-        if index[0] == len(sys.argv):
-            break
-
-        argument = sys.argv[index[0]]
-
-        if argument == "+":
-            index[0] += 1
-
-            num_2 = multiplicative(index)
-
-            if not is_valid(num_2):
-                return {"valid": False, "result": None}
-
-            num_1 = {"valid": True, "result": num_1["result"] + num_2["result"]}
-
-            continue
-        elif argument == "-":
-            index[0] += 1
-
-            num_2 = multiplicative(index)
-
-            if not is_valid(num_2):
-                return {"valid": False, "result": None}
-
-            num_1 = {"valid": True, "result": num_1["result"] - num_2["result"]}
-
-            continue
-        else:
-            break
-
-    return num_1
+            return False, None
+    except IndexError:
+        return True, eval("".join(index2 for index2 in term))
 
 
-def expression(index: list):
-    return additive(index)
-
-
-def parse():
-    return expression([1])
-
-
-print(parse())
+term = "".join(index2 for index2 in sys.argv[1:])
+print(check_result(term) if term else (False, None))
