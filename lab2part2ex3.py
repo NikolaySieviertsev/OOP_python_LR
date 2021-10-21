d8d75588e0a97dac95a620246aadeb182724fbd6
NikolaySieviertsev <90578332+NikolaySieviertsev@users.noreply.github.com>
2021-10-21 05:03:46 +0300

    first commit

---
+++ lab2part2ex3.py
@@ -0,0 +1,129 @@
+"""
+Task 3.
+The class GROUP contains a sequence of instances of the class STUDENT. The class STUDENT contains
+the student's name, surname, record book number and grades. Determine the required attributes-data
+and attributes-methods in classes GROUP and STUDENT. Find the average score of each student. Output
+to the standard output stream the five students with the highest average score.
+Assume that there can be no more than 20 students in a group, as well as students with the same name and surname.
+"""
+
+
+class Student(object):
+    """ Class Student contains the student's name, surname, record book number and grades.
+         Also it has getter and setter methods for appropriate attributes and a method to find average scores."""
+
+    def __init__(self, name, surname, record_book, *grades):
+        self.name = name
+        self.surname = surname
+        self.record_book = record_book
+        self.grades = grades
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if not isinstance(name, str):
+            raise TypeError('Surname has to be string type!')
+        if name == '':
+            raise ValueError('Name cannot be empty!')
+        self.__name = name
+
+    @property
+    def surname(self):
+        return self.__surname
+
+    @surname.setter
+    def surname(self, surname):
+        if not isinstance(surname, str):
+            raise TypeError('Surname has to be string type!')
+        if surname == '':
+            raise ValueError('Surname cannot be empty!')
+        self.__surname = surname
+
+    @property
+    def record_book(self):
+        return self.__record_book
+
+    @record_book.setter
+    def record_book(self, record_book):
+        if not isinstance(record_book, int):
+            raise TypeError('Record book number has to be int type! ')
+        self.__record_book = record_book
+
+    @property
+    def grades(self):
+        return self.__grades
+
+    @grades.setter
+    def grades(self, grades):
+        if not all(isinstance(index, int) for index in grades):
+            raise TypeError('Grade have to be int type!')
+        if not all(grades):
+            raise ValueError('Grades sequence cannot be empty!')
+        self.__grades = grades
+        self.__average_score = round(sum(grades) / len(grades), 1)
+
+    @property
+    def average_score(self):
+        return self.__average_score
+
+    def __str__(self) -> str:
+        return f'Name: {self.name}\nSurname: {self.surname}\nRecord book number: {str(self.record_book)}\n' \
+               f'Grades: {self.grades}\nAverage: {self.average_score}\n\n'
+
+
+class Group(object):
+    """" Class Group contains a sequence of instances of the class Student.
+          Also it contains list of students and method to find students with the highest average score."""
+
+    def __init__(self, *students):
+        self.students = students
+
+    @property
+    def students(self):
+        return self.__students
+
+    @students.setter
+    def students(self, student_list):
+        if not student_list:
+            raise ValueError('Group cannot be empty!')
+        if len(student_list) > 20:
+            raise ValueError('Group have to contain less than 20 students!')
+        if isinstance(all(student_list), Student):
+            raise TypeError('Students have to be "Student" type!')
+        if self.duplicates(student_list):
+            raise ValueError('Group cannot have students with the same names and surnames!')
+        self.__students = list(student_list)
+
+    def best_student(self):
+        self.students.sort(key=lambda x: x.average_score, reverse=True)
+
+    @staticmethod
+    def duplicates(student_list):
+        counter = 0
+        names = set()
+        for i in student_list:
+            names.add(i.name + i.surname)
+            counter += 1
+        if counter > len(names):
+            return True
+        return False
+
+    def __str__(self):
+        string = ""
+        for index in self.students[:5]:
+            string += str(index) + "\n"
+        return f'{string}'
+
+
+instance1 = Student('Kolya', 'Severtsev', 1, 5, 1, 5, 2, 5, 5)
+instance2 = Student('Max', 'Alexenko', 3, 5, 4, 3, 3, 2, 1)
+instance3 = Student('Dima', 'Kashuba', 2, 5, 5, 3, 1, 2, 5)
+instance4 = Student('Vlad', 'Leonenko', 4, 1, 5, 3, 3, 2, 1)
+instance5 = Student('Oleh', 'Oliynik', 5, 5, 5, 5, 5, 5, 5)
+group = Group(instance1, instance2, instance3, instance4, instance5)
+group.best_student()
+print(group)
+# print(instance3)
---
+++ lab2part2ex4.py
@@ -0,0 +1,75 @@
+"""
+Task 4.
+Create a class BINARY TREE that contains background information of product prices (product code, price of 1 product).
+The tree is sorted by product codes. From the keyboard enter data on the number of products in the format:
+product code, number of products. Using a tree, find the cost of products (cost = quantity * price of one product).
+"""
+
+
+class Node:
+    """ Class Node is a necessary part of a binary tree.
+        This class contains background information of product prices (product code, price of 1 product).
+        Also it contains a method to find the cost of products."""
+
+    def __init__(self, code, number, price, left=None, right=None):
+        self.left = left
+        self.right = right
+        self.code = code
+        self.number = number
+        self.price = price
+
+    def final_price(self, current):
+        if current.left is None and current.right is None:
+            return current.number * current.price
+        elif current.left is not None and current.right is not None:
+            return current.number * current.price + self.final_price(current.left) + self.final_price(current.right)
+        elif current.left is not None and current.right is None:
+            return current.number * current.price + self.final_price(current.left)
+        elif current.right and current.left is None:
+            return current.number * current.price + self.final_price(current.right)
+
+
+class BinaryTree:
+    """ Class BinaryTree contains method to build a binary tree."""
+
+    def __init__(self):
+        self.root = None
+
+    def insertion(self, code, number, price):
+        if not isinstance(code, int):
+            raise TypeError("Code has incorrect type for inserting!")
+        if not isinstance(number, int):
+            raise TypeError("Number has incorrect type for inserting!")
+        if not isinstance(price, int):
+            raise TypeError("Price has incorrect type for inserting!")
+        if self.root is None:
+            self.root = Node(code, number, price)
+        else:
+            current = self.root
+            while 1:
+                if code < current.code:
+                    if current.left is None:
+                        current.left = Node(code, number, price)
+                        break
+                    else:
+                        current = current.left
+                elif code > current.code:
+                    if current.right is None:
+                        current.right = Node(code, number, price)
+                        break
+                    else:
+                        current = current.right
+                else:
+                    raise ValueError("Detected an incorrect code!")
+
+
+tree = BinaryTree()
+tree.insertion(1, 5, 2)
+tree.insertion(2, 3, 3)
+tree.insertion(3, 6, 2)
+tree.insertion(4, 1, 5)
+tree.insertion(5, 2, 4)
+tree.insertion(6, 2, 4)
+tree.insertion(7, 2, 2)
+tree.insertion(8, 1, 1)
+print('The cost of all products : ', tree.root.final_price(tree.root))
